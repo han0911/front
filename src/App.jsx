2166fc84c7284baded8401d4a570264487f595e6
han0911 <a01099249559@gmail.com>
2025-08-05 00:38:39 +0900

    리액트

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Fpage from "./component/Fpage";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Fpage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Fpage.jsx
@@ -0,0 +1,12 @@
+function Fpage() {
+  return (
+    <div className="FC">
+      <div className="L"></div>
+      <div className="M">
+        <img src="Pooming.png"></img>
+      </div>
+      <div className="R"></div>
+    </div>
+  );
+}
+export default Fpage
