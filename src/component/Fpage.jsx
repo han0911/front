ccd6e132be13386e68363126d29c208160877bcd
han0911 <a01099249559@gmail.com>
2025-08-05 13:55:20 +0900

    “reactM”

--- src/component/Fpage.jsx
+++ src/component/Fpage.jsx
@@ -1,12 +1,41 @@
+// Fpage.js
+import React, { useState } from "react";
+import PoomingImage from "./Pooming.png";
+
+
 function Fpage() {
+  const [showMain, setShowMain] = useState(false);
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  const handleClick = () => {
+    setIsTransitioning(true);
+    setTimeout(() => {
+      setShowMain(true);
+    }, 500); // 0.5초 후에 메인 페이지 표시
+  };
+
+  if (showMain) {
+    return (
+      <div className="main-page">
+        <h1>메인 페이지</h1>
+        <p>여기에 원하는 콘텐츠를 추가하세요!</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="FC">
+    <div 
+      className={`FC ${isTransitioning ? 'transitioning' : ''}`}
+      onClick={handleClick}
+    >
       <div className="L"></div>
       <div className="M">
-        <img src="Pooming.png"></img>
+        <img src={PoomingImage} alt="Pooming" className="pooming-img" />
+         <div className="click-placeholder">Click to Continue</div>
       </div>
       <div className="R"></div>
     </div>
   );
 }
-export default Fpage
+
+export default Fpage;
